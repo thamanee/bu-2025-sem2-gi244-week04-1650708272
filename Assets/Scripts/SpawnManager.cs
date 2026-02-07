1413013691d22d37bdf68d4d42afc59df3703e09
M1KZER <102239415+thamanee@users.noreply.github.com>
2026-02-07 14:18:15 +0700

    Work in Progress

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] dogPrefabs;
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            float x = Random.Range(-10,10);
+            int index = Random.Range(0, dogPrefabs.Length);
+            if (dogPrefabs[index] != null)
+            {
+                Instantiate(dogPrefabs[index], new Vector3(x, 0, 20), Quaternion.Euler(0, 180, 0));
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"dog prefab {index} is null")
+        }
 
     }
 }
